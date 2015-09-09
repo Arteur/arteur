ff754b64b66ba1c84b20507d246c14813affd26c
Anita <a.nita@hotmail.co.uk>
2015-09-09 19:01:06 +0100

    form on create review page

---
+++ assets/js/main.js
@@ -0,0 +1,10 @@
+(function () {
+
+    'use strict';
+    $( document ).ready(function () {
+        $('#permanent').change(function() {
+            console.log('fdsfsdf');
+        });
+    });
+
+})();
