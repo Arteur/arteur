edcad2c815b1961ec37d3a1b2afc861f99be3338
SimonLab <simon.labondance@gmail.com>
2015-09-10 09:24:02 +0100

    form review

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,9 +2,27 @@
 
     'use strict';
     $( document ).ready(function () {
+
         $('#permanent').change(function() {
-            console.log('fdsfsdf');
+            console.log($(this).val());
+            console.log($(this).prop('checked'));
+            if($(this).prop('checked')) {
+                $('#datepicker').val('');
+                $('#datepicker').prop('disabled', true);
+            }else {
+                $('#datepicker').prop('disabled', false);
+            }
         });
+
+        $('#datepicker').datepicker({
+            dateFormat: 'dd-mm-yy',
+        });
+
+        $('#datepicker').keypress(function (e) {
+            e.preventDefault();
+        });
+        // $('.mySelect').selectpicker();
     });
+
 
 })();
