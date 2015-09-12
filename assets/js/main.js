62ac0420529ae77f9ef909cc5a99ef1215b0bba9
Anita <a.nita@hotmail.co.uk>
2015-09-13 00:54:14 +0100

    styled create review page

--- assets/js/main.js
+++ assets/js/main.js
@@ -27,5 +27,21 @@
         });
 
         $('[data-toggle="tooltip"]').tooltip();
+
+        $('a').imageLightbox(
+            {
+                selector:       'id="imagelightbox"',   // string;
+                allowedTypes:   'png|jpg|jpeg|gif',     // string;
+                animationSpeed: 250,                    // integer;
+                preloadNext:    true,                   // bool;            silently preload the next image
+                enableKeyboard: true,                   // bool;            enable keyboard shortcuts (arrows Left/Right and Esc)
+                quitOnEnd:      false,                  // bool;            quit after viewing the last image
+                quitOnImgClick: false,                  // bool;            quit when the viewed image is clicked
+                quitOnDocClick: true,                   // bool;            quit when anything but the viewed image is clicked
+                onStart:        false,                  // function/bool;   calls function when the lightbox starts
+                onEnd:          false,                  // function/bool;   calls function when the lightbox quits
+                onLoadStart:    false,                  // function/bool;   calls function when the image load begins
+                onLoadEnd:      false                   // function/bool;   calls function when the image finishes loading
+            });
     });
 })();
