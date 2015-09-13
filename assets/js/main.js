a185ebf1c85b2d36c9182dc38e3e309b193b57a1
Anita <a.nita@hotmail.co.uk>
2015-09-13 12:18:18 +0100

    slide gallery styled

--- assets/js/main.js
+++ assets/js/main.js
@@ -28,20 +28,15 @@
 
         $('[data-toggle="tooltip"]').tooltip();
 
-        $('a').imageLightbox(
-            {
-                selector:       'id="imagelightbox"',   // string;
-                allowedTypes:   'png|jpg|jpeg|gif',     // string;
-                animationSpeed: 250,                    // integer;
-                preloadNext:    true,                   // bool;            silently preload the next image
-                enableKeyboard: true,                   // bool;            enable keyboard shortcuts (arrows Left/Right and Esc)
-                quitOnEnd:      false,                  // bool;            quit after viewing the last image
-                quitOnImgClick: false,                  // bool;            quit when the viewed image is clicked
-                quitOnDocClick: true,                   // bool;            quit when anything but the viewed image is clicked
-                onStart:        false,                  // function/bool;   calls function when the lightbox starts
-                onEnd:          false,                  // function/bool;   calls function when the lightbox quits
-                onLoadStart:    false,                  // function/bool;   calls function when the image load begins
-                onLoadEnd:      false                   // function/bool;   calls function when the image finishes loading
-            });
+        $(".carousel-inner").swiperight(function() {
+            $(this).parent().carousel('prev');
+        });
+        $(".carousel-inner").swipeleft(function() {
+            $(this).parent().carousel('next');
+        });
+
+        $('#showMore').click(function () {
+            $('#moreReviews').slideToggle();
+        });
     });
 })();
