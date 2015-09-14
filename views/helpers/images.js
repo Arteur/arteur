73a90ad1284ece8f6150ed1c7fb01864a4333cb3
SimonLab <simon.labondance@gmail.com>
2015-09-14 15:39:24 +0100

    slider

--- views/helpers/images.js
+++ views/helpers/images.js
@@ -1,7 +1,14 @@
 module.exports = function(pictures) {
     var result = '';
-    pictures.forEach(function (picture) {
-        result += "<img src='" + picture + "'alt='gallery picture' class='active'>";
-    });
+    for(var i = 0; i < pictures.length; i++) {
+
+        if (i === 0 ) {
+
+            result = result +  "<div class='item active'><img src='" + pictures[i] + "'></div>";
+        }else {
+            result = result + "<div class='item'><img src='" + pictures[i] + "'></div>";
+        }
+    }
+
     return result;
 };
