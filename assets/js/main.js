01978d29df72c01ba3cbf400062b630aa6bfbdb2
Anita <a.nita@hotmail.co.uk>
2015-09-14 21:05:46 +0100

    map page

--- assets/js/main.js
+++ assets/js/main.js
@@ -77,4 +77,24 @@
             }
         });
     });
+    function initialize() {
+        var mapCanvas = document.getElementById('map');
+        var mapOptions = {
+            center: new google.maps.LatLng(51.515514, -0.096721),
+            zoom: 15,
+            mapTypeId: google.maps.MapTypeId.ROADMAP
+        };
+        var map = new google.maps.Map(mapCanvas, mapOptions);
+
+        $(window).resize(function() {
+            // (the 'map' here is the result of the created 'var map = ...' above)
+            google.maps.event.trigger(map, "resize");
+        });
+        var marker = new google.maps.Marker({
+            position: mapOptions.center,
+            map: map,
+            title: "16 St Martin's Le Grand"
+        });
+    }
+    google.maps.event.addDomListener(window, 'load', initialize);
 })();
